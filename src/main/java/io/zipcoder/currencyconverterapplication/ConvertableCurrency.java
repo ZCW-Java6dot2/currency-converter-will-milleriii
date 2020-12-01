d273bd46e1904aff4641124721cea8c692fff379
will <will@zipcodes-MBP-8.fios-router.home>
2020-11-30 21:21:11 -0500

    finished lab

--- src/main/java/io/zipcoder/currencyconverterapplication/ConvertableCurrency.java
+++ src/main/java/io/zipcoder/currencyconverterapplication/ConvertableCurrency.java
@@ -2,6 +2,16 @@ package io.zipcoder.currencyconverterapplication;
 
 public interface ConvertableCurrency {
     default Double convert(CurrencyType currencyType) {
-        return Double.MAX_VALUE;
+
+        //need (currency / universal) = val 1
+        // (currency2 / universal) = val 2
+        // return val 1 / val 2
+        Double val1 = currencyType.getRate();
+        Double val2 = this.getCurrencyType().getRate();
+        return val1 / val2;
     }
+
+    CurrencyType getCurrencyType();
+
+
 }
